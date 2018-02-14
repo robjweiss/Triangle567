601225a5cdc2dfe0259d252190c6b7220e47e8e1
robjweiss <rweiss55@gmail.com>
2018-02-13 22:50:49 -0500

    Fixed typo in import

--- TestTriangle.py
+++ TestTriangle.py
@@ -11,7 +11,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 import math
 import unittest
 
-from Triangle import classifyTriangle getInput
+from Triangle import classifyTriangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
