0fc75daeef078d37983d5a0442b84acf233a7e5a
Rob Weiss <rweiss55@gmail.com>
2018-02-27 20:52:30 -0500

    Updated names in tests

--- testTriangle.py
+++ testTriangle.py
@@ -10,13 +10,13 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 import unittest
 
-from Triangle import classify_triangle
-from Triangle import check_input
+from triangle import classify_triangle
+from triangle import check_input
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
-class TestTriangles(unittest.TestCase):
+class testTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def test_right_scalene_triangle_a(self):
