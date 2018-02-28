5bff19849739d154d78fd62706f7464ca041c882
Rob Weiss <rweiss55@gmail.com>
2018-02-27 20:47:38 -0500

    Refactored

--- TestTriangle.py
+++ TestTriangle.py
@@ -8,10 +8,10 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 @author: rjw
 """
 
-import math
 import unittest
 
-from Triangle import classifyTriangle
+from Triangle import classify_triangle
+from Triangle import check_input
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -19,59 +19,65 @@ from Triangle import classifyTriangle
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testRightScaleneTriangleA(self):
-        self.assertEqual(classifyTriangle(3,4,5),'Right Scalene','3,4,5 is a Right Scalene triangle')
+    def test_right_scalene_triangle_a(self):
+        self.assertEqual(classify_triangle(3, 4, 5), 'Right Scalene', '3,4,5 is a Right Scalene triangle')
 
-    def testRightScaleneTriangleB(self):
-        self.assertEqual(classifyTriangle(5,3,4),'Right Scalene','5,3,4 is a Right Scalene triangle')
+    def test_right_scalene_triangle_b(self):
+        self.assertEqual(classify_triangle(5, 3, 4), 'Right Scalene', '5,3,4 is a Right Scalene triangle')
 
-    def testRightScaleneTriangleC(self):
-        self.assertEqual(classifyTriangle(3.0005,4.0005,5.0005), 'Right Scalene')
+    def test_right_scalene_triangle_c(self):
+        self.assertEqual(classify_triangle(3.0005, 4.0005, 5.0005), 'Right Scalene')
 
-    def testRightIsoscelesTriangleA(self):
-        self.assertEqual(classifyTriangle(10*(2**.5), 10*(2**.5), 20), 'Right Isosceles')
+    def test_right_isosceles_triangle_a(self):
+        self.assertEqual(classify_triangle(10*(2**.5), 10*(2**.5), 20), 'Right Isosceles')
 
-    def testRightTriangleA(self):
-        self.assertNotEqual(classifyTriangle(4, 4, 4), 'Right')
+    def test_right_triangle_a(self):
+        self.assertNotEqual(classify_triangle(4, 4, 4), 'Right')
 
-    def testEquilateralTrianglesA(self):
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def test_equilateral_triangles_a(self):
+        self.assertEqual(classify_triangle(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
 
-    def testEquilateralTrianglesB(self):
-        self.assertNotEqual(classifyTriangle(4, 5, 5), 'Equilateral')
+    def test_equilateral_triangles_b(self):
+        self.assertNotEqual(classify_triangle(4, 5, 5), 'Equilateral')
 
-    def testValidA(self):
-        self.assertNotEqual(classifyTriangle(3, 4, 5), 'NotATriangle')
+    def test_equilateral_triangles_c(self):
+        self.assertNotEqual(classify_triangle(3, 4, 5), 'Equilateral')
 
-    def testValidB(self):
-        self.assertEqual(classifyTriangle(1, 2, 3), 'NotATriangle')
+    def test_valid_a(self):
+        self.assertNotEqual(check_input(3, 4, 5), 'InvalidInput')
 
-    def testInputA(self):
-        self.assertEqual(classifyTriangle(201, 203, 257), 'InvalidInput')
+    def test_valid_b(self):
+        self.assertEqual(check_input(1, 2, 3), 'InvalidInput')
 
-    def testInputB(self):
-        self.assertEqual(classifyTriangle(0, 0, 0), 'InvalidInput')
+    def test_input_a(self):
+        self.assertEqual(check_input(201, 203, 257), 'InvalidInput')
 
-    def testInputC(self):
-        self.assertEqual(classifyTriangle(-3, -4, -5), 'InvalidInput')
+    def test_input_b(self):
+        self.assertEqual(check_input(0, 0, 0), 'InvalidInput')
 
-    def testScaleneA(self):
-        self.assertEqual(classifyTriangle(2, 4, 5), 'Scalene')
+    def test_input_c(self):
+        self.assertEqual(check_input(-3, -4, -5), 'InvalidInput')
 
-    def testScaleneB(self):
-        self.assertNotEqual(classifyTriangle(2, 2, 1), 'Scalene')
+    def test_input_d(self):
+        self.assertEqual(check_input('notnum1', 'string', 'notnum2'), 'InvalidInput')
 
-    def testIsoscelesA(self):
-        self.assertEqual(classifyTriangle(2, 2, 1), 'Isosceles')
+    def test_scalene_a(self):
+        self.assertEqual(classify_triangle(2, 4, 5), 'Scalene')
 
-    def testIsoscelesB(self):
-        self.assertEqual(classifyTriangle(1, 3, 3), 'Isosceles')
+    def test_scalene_b(self):
+        self.assertNotEqual(classify_triangle(2, 2, 1), 'Scalene')
 
-    def testIsoscelesC(self):
-        self.assertEqual(classifyTriangle(4, 2, 4), 'Isosceles')
+    def test_isosceles_a(self):
+        self.assertEqual(classify_triangle(2, 2, 1), 'Isosceles')
 
-    def testIsoscelesD(self):
-        self.assertNotEqual(classifyTriangle(2,2,2), 'Isosceles')
+    def test_isosceles_b(self):
+        self.assertEqual(classify_triangle(1, 3, 3), 'Isosceles')
+
+    def test_isosceles_c(self):
+        self.assertEqual(classify_triangle(4, 2, 4), 'Isosceles')
+
+    def test_isosceles_d(self):
+        self.assertNotEqual(classify_triangle(2, 2, 2), 'Isosceles')
 
 
 
--- Triangle.py
+++ Triangle.py
@@ -12,7 +12,47 @@ The primary goal of this file is to demonstrate a simple python program to class
 
 import math
 
-def classifyTriangle(a,b,c):
+def check_input(side_a, side_b, side_c):
+    """
+    Verifies that input is valid and meets the appropriate criteria
+    """
+
+    try:
+        side_a = float(side_a)
+        side_b = float(side_b)
+        side_c = float(side_c)
+
+    except:
+        # verify that all 3 inputs are integers
+        # Python's "isinstance(object,type) returns True if the object is of the specified type
+        float_double_a = isinstance(side_a, (float, int))
+        float_double_b = isinstance(side_b, (float, int))
+        float_double_c = isinstance(side_c, (float, int))
+        if not(float_double_a and float_double_b and float_double_c):
+            print('Please enter real numbers only')
+            return 'InvalidInput'
+
+    # require that the input values be >= 0 and <= 200
+    if side_a > 200 or side_b > 200 or side_c > 200:
+        print('The values specified are too large')
+        return 'InvalidInput'
+
+    if side_a <= 0 or side_b <= 0 or side_c <= 0:
+        print('The values specified are too small or negative')
+        return 'InvalidInput'
+
+    # This information was not in the requirements spec but
+    # is important for correctness
+    # the sum of any two sides must be strictly less than the third side
+    # of the specified shape is not side_a triangle
+    a_largest = (side_a >= (side_b + side_c))
+    b_largest = (side_b >= (side_a + side_c))
+    c_largest = (side_c >= (side_a + side_b))
+    if a_largest or b_largest or c_largest:
+        print('The sides entered do not form a valid triangle')
+        return 'InvalidInput'
+
+def classify_triangle(side_a, side_b, side_c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of
     you test cases.
@@ -30,57 +70,43 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
-    # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        print('The values specified are too large')
-        return 'InvalidInput'
-
-    if a <= 0 or b <= 0 or c <= 0:
-        print('The values specified are too small or negative')
-        return 'InvalidInput'
-
-    # verify that all 3 inputs are integers
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,(float, int)) and isinstance(b,(float, int)) and isinstance(c,(float, int))):
-        print('Please enter real numbers only')
-        return 'InvalidInput';
-
-    # This information was not in the requirements spec but
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
-        print('The sides entered do not form a valid triangle')
-        return 'NotATriangle'
-
-    # now we know that we have a valid triangle
-    if a == b and b == c:
+    # now we know that we have side_a valid triangle
+    if side_a == side_b and side_b == side_c:
         print('Equilateral Triangle')
         return 'Equilateral'
 
-    if (math.isclose(((a ** 2) + (b ** 2)), (c ** 2), abs_tol = .01) or math.isclose(((a ** 2) + (c ** 2)), (b ** 2), abs_tol = .01) or math.isclose(((c ** 2) + (b ** 2)), (a ** 2), abs_tol = .01)):
-        if (a != b) and  (b != c) and (a != c):
+    in_range_c = math.isclose(((side_a ** 2) + (side_b ** 2)), (side_c ** 2), abs_tol=.01)
+    in_range_b = math.isclose(((side_a ** 2) + (side_c ** 2)), (side_b ** 2), abs_tol=.01)
+    in_range_a = math.isclose(((side_c ** 2) + (side_b ** 2)), (side_a ** 2), abs_tol=.01)
+    if in_range_c or in_range_b or in_range_a:
+        if (side_a != side_b) and  (side_b != side_c) and (side_a != side_c):
             print('Right Scalene Triangle')
             return 'Right Scalene'
-        elif(a == b or b == c or c == a):
+        elif(side_a == side_b or side_b == side_c or side_c == side_a):
             print('Right Isosceles Triangle')
             return 'Right Isosceles'
-        else:
-            return 'Right'
+        return 'Right'
 
-    if (a != b) and  (b != c) and (a != c):
-        return 'Scalene'
-    if(a == b or b == c or c == a):
+    if(side_a == side_b or side_b == side_c or side_c == side_a):
         print('Isosceles Triangle')
         return 'Isosceles'
 
-def getInput():
-    sides = input("Please enter the three sides of a triangle no less than 1 and no greater than 199. Seperate entries by spaces:")
+    #Deprecated
+    #if (side_a != side_b) and  (side_b != side_c) and (side_a != side_c):
+    return 'Scalene'
+
+def get_input():
+    """
+    Gets input from the user
+    """
+
+    sides = input("Enter three sides of a triangle between 1 and 199. Seperate entries by spaces:")
     return sides
 
 if __name__ == '__main__':
-    sides = getInput().split(' ')
-    a = float(sides[0])
-    b = float(sides[1])
-    c = float(sides[2])
-    classifyTriangle(a,b,c)
+    SIDES = get_input().split(' ')
+    SIDE_A = SIDES[0]
+    SIDE_B = SIDES[1]
+    SIDE_C = SIDES[2]
+    if check_input(SIDE_A, SIDE_B, SIDE_C) != 'InvalidInput':
+        classify_triangle(float(SIDE_A), float(SIDE_B), float(SIDE_C))
